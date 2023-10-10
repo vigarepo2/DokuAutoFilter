f6b163fb920c68dbe542f81e1ba9e9691fc0dfa9
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2023-10-10 12:35:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://vigarepo2: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@github.com/vigarepo2/DokuAutoFilter /Eva
+  git clone https://github.com/vigarepo2/DokuAutoFilter /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
